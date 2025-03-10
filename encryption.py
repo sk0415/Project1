aef9a4210706806278e0f07211f0e65d63bcc4bc
unknown <sanjana1kotha@gmail.com>
2025-03-09 22:06:46 -0500

    Vigenere encrpytion code from Rosetta Code

---
+++ encryption.py
@@ -0,0 +1,37 @@
+from itertools import starmap, cycle
+
+
+def encrypt(message, key):
+    '''Vigenere encryption of message using key.'''
+
+    # Converted to uppercase.
+    # Non-alpha characters stripped out.
+    message = filter(str.isalpha, message.upper())
+
+    def enc(c, k):
+        '''Single letter encryption.'''
+
+        return chr(((ord(k) + ord(c) - 2 * ord('A')) % 26) + ord('A'))
+
+    return ''.join(starmap(enc, zip(message, cycle(key))))
+
+
+def decrypt(message, key):
+    '''Vigenere decryption of message using key.'''
+
+    def dec(c, k):
+        '''Single letter decryption.'''
+
+        return chr(((ord(c) - ord(k) - 2 * ord('A')) % 26) + ord('A'))
+
+    return ''.join(starmap(dec, zip(message, cycle(key))))
+
+
+def main():
+
+    passkey = None
+
+
+
+if __name__ == '__main__':
+    main()
