9c17278e3aebc42c395ba0fb25928a5e118b70d7
unknown <sanjana1kotha@gmail.com>
2025-03-17 20:32:27 -0500

    logger formatted

--- driver.py
+++ driver.py
@@ -1,4 +1,5 @@
 import sys
+import subprocess
 from subprocess import Popen, PIPE
 
 def log_message( logger , message ):
@@ -10,6 +11,19 @@ def encryption_command( encryption , command ):
     encryption.stdin.flush()
     return encryption.stdout.readline().strip()
 
+def print_menu():
+    print("              Menu                 ")
+    print("-----------------------------------")
+    print("password   - Set password for encryption/decryption")
+    print("encrypt    - Encrypt string")
+    print("decrypt    - Decrypt string")
+    print("history    - Show history")
+    print("quit       - Quit program")
+    print("-----------------------------------")
+
+def showHistory():
+    print()
+
 def main():
     if len(sys.argv) != 2:
         print( "Usage : python driver.py <log_file_name.")
@@ -17,10 +31,38 @@ def main():
 
     log_fileName = sys.argv[1]
 
-    logger = Popen(['python' , 'logger.py' , log_fileName] , stdout = PIPE , stdin = PIPE , encoding = 'utf8')
-    encryption = Popen(['python' , 'encryption.py'] , stdout = PIPE , stdin = PIPE , encoding = 'utf8')
+    logger = subprocess.Popen(['python' , 'logger.py' , log_fileName] , stdout = subprocess.PIPE , stdin = subprocess.PIPE , encoding = 'utf8')
+    encryption = subprocess.Popen(['python' , 'encryption.py'] , stdout = subprocess.PIPE , stdin = subprocess.PIPE , encoding = 'utf8')
 
-    log_message( logger , "START : Driver program started.")
+    log_message( logger , "START Driver program started.")
+
+    while True:
+        print_menu()
+
+        command = input( "Enter command: ").strip().lower()
+
+        if command == 'password':
+            print()
+            
+        elif command == 'encrypt':
+            print()
+
+        elif command == 'decrypt':
+            print()
+
+        elif command == 'history':
+            showHistory()
+
+        elif command == 'quit':
+            encryption_command( encryption , command )
+            log_message( logger , "STOP Program terminated." )
+            break
+
+        else:
+            print( "Invalid command. Try again" )
+
+    logger.terminate
+    encryption.terminate
 
 if __name__ == '__main__':
     main()
--- logger.py
+++ logger.py
@@ -1,4 +1,5 @@
 import sys
+import datetime
 
 def main():
     if len(sys.argv) != 2:
@@ -6,6 +7,29 @@ def main():
         sys.exit(1)
 
     log_file = sys.argv[1]
+
+    with open(log_file, "a") as f:
+        while True:
+            try:
+                line = input().strip()
+                if not line:
+                    continue
+
+                if line.startswith("QUIT"):
+                    break 
+                
+                current_time = datetime.datetime.now().strftime( "%Y-%m-%d %H:%M" )
+
+                words = line.split()
+                action = words[0]
+                message = ' '.join( words[1:] )
+
+                newLine = current_time + ' [' + action + '] ' + message
+
+                f.write(newLine + "\n")
+                f.flush()
+            except EOFError:
+                break 
 
 if __name__ == "__main__":
     main()
